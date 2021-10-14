6ce47785b941c2f7ab25f2a3f1ba1587d80539d8
lionelxchen <lionelxchen@tencent.com>
2021-10-14 23:40:27 +0800

    resolve conflicts

--- tools/install.sh
+++ tools/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv /usr/bin/nvidia-container-runtime-hook        /usr/bin/nvidia-container-runtime-hook-bak
-mv /host/usr/bin/mount_nano_gpu                  /host/usr/bin/mount_nano_gpu-bak
+mv /host/usr/bin/nvidia-container-runtime-hook   /usr/bin/nvidia-container-runtime-hook-bak
+mv /host/usr/bin/mount_nano_gpu                  /usr/bin/mount_nano_gpu-bak
 cp /usr/bin/hook                                 /host/usr/bin/nvidia-container-runtime-hook
 cp /usr/bin/mount_nano_gpu                       /host/usr/bin/mount_nano_gpu
