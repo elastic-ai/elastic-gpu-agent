260910781d49ce2a639d0c890c3eb11ad75935af
xiaoxubeii <xiaoxubeii@gmail.com>
2022-05-07 12:41:06 +0800

    Fix symlink error.

--- tools/install.sh
+++ tools/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cp /usr/bin/egpu-nvidia-container-runtime-hook /host/usr/bin/nvidia-container-runtime-hook
-cp /usr/bin/egpu-nvidia-container-toolkit /host/usr/bin/nvidia-container-toolkit
+mv /host/usr/bin/nvidia-container-runtime-hook /host/usr/bin/nvidia-container-runtime-hook-bak
+mv /host/usr/bin/nvidia-container-toolkit /host/usr/bin/nvidia-container-toolkit-bak
+cp --preserve=timestamps /usr/bin/egpu-nvidia-container-runtime-hook /host/usr/bin/nvidia-container-runtime-hook
+cp --preserve=timestamps /usr/bin/egpu-nvidia-container-toolkit /host/usr/bin/nvidia-container-toolkit
